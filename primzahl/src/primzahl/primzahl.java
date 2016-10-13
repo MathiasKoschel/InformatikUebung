cd30227bbfcf68a602df5d525e33fbec9b781618
Koschel.Mathias <Koschel.Mathias@SK-D8CB8A4FAAFF.2631.skool.net>
2016-10-13 09:13:24 +0200

    00

---
+++ primzahl/src/primzahl/primzahl.java
@@ -0,0 +1,66 @@
+package primzahl;
+
+public class primzahl {
+
+	static int number = 8;
+	
+	public static void main(String[] args) {
+		
+		System.out.println(addiere1(1,2));}
+		
+		
+		public static int addiere1(int zahl1, int zahl2) {
+			return zahl1+zahl2;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	/*	// TODO Auto-generated method stub
+				int Ausgabe=0;
+			for (int i=2; i<=10000; i++){
+				number=i;
+			if(isPrime(number)==true){
+					System.out.print(i +",");
+				Ausgabe++;
+				if(Ausgabe%25==0){
+					System.out.println("");
+				
+				Ausgabe=0;
+					}
+			}
+			}
+	*/}
+
+	private static char[] addiere(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+		
+	/* public static boolean isPrime(int number){
+			
+			if(number<2){return false;}
+			
+			
+			for(int i=2; i<=number/2; i++){
+				if(number%i==0){
+					return false;
+					}
+			}
+				return true;
+				}  */
+			}
+		
+
